5f0f0a37dd6a469b9be16a7aaf50bfddf3181c42
Jahidul96 <jahidul59895@gmail.com>
2024-04-04 03:05:39 +0600

    added home page content

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
-import Leaf from './Leaf';
+import "./App.css";
+import Leaf from "./Leaf";
+import Demo from "./app/screens/demo/Demo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <Leaf/>
-      </header>
-    </div>
-  );
+    return (
+        // <div className="App">
+        //     {/* <header className="App-header"> */}
+        //     {/* <Leaf/> */}
+        //     <Demo />
+        //     {/* </header> */}
+        // </div>
+        <div>
+            <Demo />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/app/components/PersonCard.js
@@ -0,0 +1,41 @@
+import {
+    Box,
+    Button,
+    ButtonGroup,
+    Card,
+    CardBody,
+    CardFooter,
+    Divider,
+    Heading,
+    Image,
+    Stack,
+    Text,
+} from "@chakra-ui/react";
+import React from "react";
+
+const PersonCard = () => {
+    return (
+        <Card w="60%" h="85%" bg="white" borderRadius={0}>
+            <Image
+                src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                alt="Green double couch with wooden legs"
+                h={"45%"}
+            />
+            <Box px="10px" py="10px">
+                <Text fontSize="18px" fontWeight="bold">
+                    John doe
+                </Text>
+                <Text fontSize="13px" my="5px">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry
+                </Text>
+
+                <Button mt="5px" bg={"blue"} color={"white"}>
+                    Click me
+                </Button>
+            </Box>
+        </Card>
+    );
+};
+
+export default PersonCard;
